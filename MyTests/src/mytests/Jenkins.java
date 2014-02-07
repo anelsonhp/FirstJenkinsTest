8d27dc1deee6111c8622ec7abaafa1e68003c76e
anelsonhp <adam.nelson@hp.com>
2014-02-07 13:29:43 -0500

    first commit

---
+++ MyTests/src/mytests/Jenkins.java
@@ -0,0 +1,16 @@
+package mytests;
+
+public class Jenkins {
+  
+  public static void main(String[] args) {
+    new Jenkins().print();
+  }
+  
+  public Jenkins() {
+    super();
+  }
+  
+  public void print() {
+    System.out.println("TEST");
+  }
+}
