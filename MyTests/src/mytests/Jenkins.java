2a3012f79cba1ea09e425b6563d6a4af1932e58f
anelsonhp <adam.nelson@hp.com>
2014-02-10 15:30:42 -0500

    BKLOG-13
    Test again

--- MyTests/src/mytests/Jenkins.java
+++ MyTests/src/mytests/Jenkins.java
@@ -13,6 +13,6 @@ public class Jenkins {
   }
   
   public void print() {
-    System.out.println(DigestUtils.sha256Hex("TESTTEST"));
+    System.out.println(DigestUtils.sha256Hex("TESTTEST2"));
   }
 }
